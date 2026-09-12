e9d0b0f0b3bf321411265f2d0dc9e34060fcb6fd
Currentflow1 <leonardB1706@gmail.com>
2026-09-12 22:13:53 +0800

    Submit Research

--- org/admin.py
+++ org/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Announcement
 
+
 @admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = (
@@ -10,18 +11,15 @@ class AnnouncementAdmin(admin.ModelAdmin):
         "is_published",
         "created_at",
     )
-
     list_filter = (
         "is_published",
         "published_at",
     )
-
     search_fields = (
         "title",
         "summary",
         "content",
     )
-
     prepopulated_fields = {
         "slug": ("title",),
     }
--- research/admin.py
+++ research/admin.py
@@ -1,8 +1,66 @@
 from django.contrib import admin
 
-from .models import ResearchField, ResearchTitle, ResearchPaper
+from .models import ResearchField, ResearchPaper, ResearchTitle
 
 
-admin.site.register(ResearchField)
-admin.site.register(ResearchTitle)
-admin.site.register(ResearchPaper)
+@admin.register(ResearchField)
+class ResearchFieldAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+
+
+@admin.register(ResearchTitle)
+class ResearchTitleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "research_field",
+        "publication_status",
+        "school_year",
+        "created_at",
+    )
+    list_filter = (
+        "publication_status",
+        "research_field",
+        "school_year",
+    )
+    search_fields = (
+        "title",
+        "authors",
+        "keywords",
+        "department",
+        "program",
+        "adviser",
+    )
+    list_select_related = (
+        "research_field",
+        "submitted_by",
+    )
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+
+@admin.register(ResearchPaper)
+class ResearchPaperAdmin(admin.ModelAdmin):
+    list_display = (
+        "research_title",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "research_title__title",
+        "abstract",
+    )
+    list_select_related = (
+        "research_title",
+    )
