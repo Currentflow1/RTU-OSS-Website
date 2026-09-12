0ec3bbbd33c1e06131744605889ba0dc25ce39bb
Currentflow1 <leonardB1706@gmail.com>
2026-09-12 12:46:08 +0800

    Update production domain configuration

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("research.urls")),
-    path('org/', include('org.urls')),
+    path("research/", include("research.urls")),
+    path("", include('org.urls')),
 ]
 
 if settings.DEBUG:
